514ce2906bd5bbc1cc965d7309e2a37c9966a6c2
Abraham <wits73@hotmail.com>
2018-09-03 19:39:13 -0400

    added palette

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,27 +2,27 @@ import React, { Component } from "react";
 import "./TodoItem.css";
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
   render() {
-    const { text, checked, id, onToggle, onRemove } = this.props;
+    const { text, checked, id, onToggle, onRemove, color } = this.props;
     console.log(id);
 
     return (
-      <div className="todo-item" onClick={() => onToggle(id)}>
-        <div
-          className="remove"
-          onClick={e => {
+        <div className="todo-item" onClick={() => onToggle(id)}>
+          <div className="remove" onClick={(e) => {
             e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-            onRemove(id);
-          }}
-        >
-          &times;
+            onRemove(id)}
+          }>&times;</div>
+          <div style={{ color }} className={`todo-text ${checked && 'checked'}`}>
+            <div>{text}</div>
+          </div>
+          {
+            checked && (<div className="check-mark">&#x2713;</div>)
+          }
         </div>
-        <div className={`todo-text ${checked && "checked"}`}>
-          <div>{text}</div>
-        </div>
-        {checked && <div className="check-mark">✓</div>}
-      </div>
-    );
+      );
   }
 }
 
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 
 class TodoItemList extends Component {
-  shouldComponentUpdate(nextProps) {
+  shouldComponentUpdate(nextProps, nextState) {
     return this.props.todos !== nextProps.todos;
   }
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
-    const todoList = todos.map(({ id, text, checked }) => (
+    const todoList = todos.map(({ id, text, checked, color }) => (
       <TodoItem
         id={id}
         text={text}
         checked={checked}
+        color={color}
         onToggle={onToggle}
         onRemove={onRemove}
         key={id}
